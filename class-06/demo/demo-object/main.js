9e2ad257e7acb56cccdaac7e0c5d8fd06e0e52dd
khoitq-acl <khoitq@americancodelab.com>
2023-06-02 13:44:41 +0700

    Add class 06

---
+++ class-06/demo/demo-object/main.js
@@ -0,0 +1,46 @@
+const manh = {
+  // thuộc tính: properties
+  tay: 2,
+  chan: 2,
+
+  // phương thức: method
+  hoc: function () {
+    console.log("Tôi đang học FE");
+  }
+}
+
+const sang = {
+  // thuộc tính: properties
+  tay: 2,
+  chan: 2,
+
+  laptop: 2,
+
+  // phương thức: method
+  hoc: function () {
+    console.log("Tôi đang học FE");
+  }
+}
+
+
+
+const dragonFruitYakult = {
+  image: 'https://haanh-180197.github.io/Final-Project/image-banner-menu/menu/%C4%91%E1%BB%93%20u%E1%BB%91ng/yakul-1-cd6d.jpg',
+  name: 'Dragon Fruit Yakult',
+  price: '49.000',
+}
+
+const traSuaTasty = {
+  image: 'https://haanh-180197.github.io/Final-Project/image-banner-menu/menu/%C4%91%E1%BB%93%20u%E1%BB%91ng/tra-sua-tasty-1280x1000-9a3c.jpg',
+  name: 'Trà sữa TASTY',
+  price: '39.000',
+}
+
+
+const bacXiuTasty = {
+  image: 'https://haanh-180197.github.io/Final-Project/image-banner-menu/menu/%C4%91%E1%BB%93%20u%E1%BB%91ng/bac-xiu-tasty-1280x1000-98cd.jpg',
+  name: 'Bạc xỉu TASTY',
+  price: '39.000',
+}
+
+
